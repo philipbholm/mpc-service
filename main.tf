40c27a98b5216b22cd2c1a38d5476294583edb6f
Philip Holm <philipbaltzersen@gmail.com>
2024-09-09 14:27:20 +0200

    Add internet gateway to vpc

--- main.tf
+++ main.tf
@@ -15,6 +15,25 @@ resource "aws_subnet" "main" {
     vpc_id = aws_vpc.main.id
     cidr_block = "172.16.0.0/24"
     availability_zone = "eu-central-1a"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "main" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main.id
+    }
+}
+
+resource "aws_route_table_association" "main" {
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.main.id
 }
 
 resource "aws_security_group" "main" {
