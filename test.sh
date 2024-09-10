606e047bf4374aec64ee6e5d74265a2d6a1d1eac
Philip Holm <philipbaltzersen@gmail.com>
2024-09-10 20:27:43 +0200

    Use nitro-console in test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 docker build -t server src/enclave
 nitro-cli build-enclave --docker-uri server --output-file server.eif
 nitro-cli run-enclave --eif-path server.eif --cpu-count 2 --memory 2048 --enclave-cid 16 --debug-mode
-echo $(nitro-cli describe-enclaves)
+nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
