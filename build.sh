ad7eeef489d402466ea48b79effefa051b6b88dd
Philip Holm <philipbaltzersen@gmail.com>
2024-09-11 21:34:55 +0200

    Add no-cache to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t enclave_base src/enclave/base_image
-docker build -t server src/enclave
-docker build -t builder .
+docker build -t enclave_base --no-cache src/enclave/base_image
+docker build -t server --no-cache src/enclave
+docker build -t builder --no-cache .
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/build:/build -it builder
