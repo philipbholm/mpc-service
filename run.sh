eee411de30c711d27524df1b02941f9b14edd967
Philip Holm <philipbaltzersen@gmail.com>
2024-09-12 15:09:47 +0200

    Make enclave run with CID 16

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t server src/enclave
 nitro-cli build-enclave --docker-uri server --output-file server.eif
-nitro-cli run-enclave --eif-path server.eif --cpu-count 1 --memory 2048 --debug-mode
+nitro-cli run-enclave --eif-path server.eif --cpu-count 1 --memory 2048 --enclave-cid 16 --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r '.[0].EnclaveID')
