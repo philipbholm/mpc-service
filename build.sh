b50d4bf18749277ee63e22a8b7ecdacc373a3c6f
Philip Holm <philipbaltzersen@gmail.com>
2024-11-12 09:18:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,42 +2,36 @@
 
 set -e
 
-# INFO: 
-# https://medium.com/nttlabs/bit-for-bit-reproducible-builds-with-dockerfile-7cc2b9faed9f
-# https://reproducible-builds.org/
+# Supported architectures: arm64, amd64
+TARGET_ARCH="arm64"
 
 # TODO:
 # - Make build reproducible on mac without python dependencies 
 # - Add dependecies from pip
-# - Ensure consistent results with remote instance
 
 echo "Preparing builder for enclave image."
 
-# Run buildkit with a specific version inside docker
-docker run -d --name buildkitd --privileged moby/buildkit:v0.16.0
-export BUILDKIT_HOST=docker-container://buildkitd
 # Or set the current builder to use a specific version
 docker buildx create \
     --driver=docker-container \
-    --driver-opt image=moby/buildkit:v0.16.0 \
-    --use
+    --driver-opt image=moby/buildkit:v0.17.0 \
+    --name repro
 
-# Build enclave image locally and add image sha to config.tfvars
-# RUN find /app -exec touch -t 202401010000.00 {} +
-# TODO: Is --no-cache needed?
-docker build -t local2 -f debian.Dockerfile --build-arg SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct) .
-diffoci diff 
-
-
-docker build -t local2 \
-    -f debian.Dockerfile \
-    --platform linux/arm64 \
-    --build-arg SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct) \
-    .
-
+docker buildx --builder repro \
+    build \
+    -t server \
+    --platform linux/$TARGET_ARCH \
     --no-cache \
-    --output type=image,rewrite-timestamp=true \
-docker build -t builder - <<EOF
+    --build-arg SOURCE_DATE_EPOCH=0 \
+    --output type=docker,rewrite-timestamp=true \
+    src/enclave
+
+# Enable QEMU if necessary
+# https://github.com/multiarch/qemu-user-static
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# Build enclave image locally
+docker build -t builder --platform linux/$TARGET_ARCH - <<EOF
 FROM amazonlinux:2023
 
 RUN dnf install -y jq aws-nitro-enclaves-cli aws-nitro-enclaves-cli-devel
@@ -45,8 +39,26 @@ RUN dnf install -y jq aws-nitro-enclaves-cli aws-nitro-enclaves-cli-devel
 CMD ["/bin/bash", "-c", "nitro-cli build-enclave --docker-uri server --output-file dummy.eif | jq --raw-output '.Measurements.PCR0'"]
 EOF
 
-# TODO: Set hash on base image if necessary
-# TODO: Set versions of nitro-cli
-
 echo "Building enclave image file." 
 enclave_image_sha=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock builder)
+
+# TODO: Add cleanup
+
+
+# Temporary commands
+docker build -t builder --platform linux/arm64 .
+
+docker run --rm -it --platform linux/arm64 -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/workspace builder
+
+nitro-cli build-enclave --docker-uri docker --output-file out.eif
+
+# Compare differences
+docker save -o remote.tar remote
+scp -i ~/.ssh/mpc-key.pem ec2-user@$(tofu -chdir=infra output -raw public_ip):/home/ec2-user/mpc-service/remote.tar ./
+docker load -i remote.tar
+diffoci diff --semantic docker://local docker://remote
+
+# Ubuntu ARM
+ssh -i "sandbox.pem" ubuntu@18.185.248.204
+scp -i ~/.ssh/sandbox.pem ubuntu@18.185.248.204:/home/ubuntu/mpc-service/oci-ubu.tar ./
+scp -i ~/.ssh/sandbox.pem ubuntu@18.185.248.204:/home/ubuntu/mpc-service/tar-ubu.tar ./
