f71d8d7d8bd028f564ebe8c2e975f440dd692935
Philip Holm <philipbaltzersen@gmail.com>
2024-11-19 08:08:23 +0100

    Update run.sh

--- attestation/server/run.sh
+++ attestation/server/run.sh
@@ -4,8 +4,5 @@
 ip link set lo up
 ip addr add 127.0.0.1/8 dev lo
 
-# Add a hosts record, pointing API endpoint to local loopback
-echo "127.0.0.1   kms.us-east-1.amazonaws.com" >> /etc/hosts
-
-nohup python3 /app/proxy.py 443 3 8000 &
-python3 /app/server.py
+python /app/proxy.py 443 3 8000 &
+python /app/server.py
