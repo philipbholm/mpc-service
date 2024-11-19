e6ad3e80745a68f08a2965b894ba82ee739e0ab7
Philip Holm <philipbaltzersen@gmail.com>
2024-11-19 09:16:02 +0100

    Remove loopback since it was configured correctly

--- attestation/server/run.sh
+++ attestation/server/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-# Assign an IP address to local loopback
-ip link set lo up
-ip addr add 127.0.0.1/8 dev lo
-
-python /app/proxy.py 443 3 8000 &
+nohup python /app/proxy.py 443 3 8000 &
 python /app/server.py
