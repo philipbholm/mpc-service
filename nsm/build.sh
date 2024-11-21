14c632482d197f49a913ba870912fc9f7525fe51
Philip Holm <philipbaltzersen@gmail.com>
2024-11-21 14:03:52 +0100

    Only suppress docker output

--- nsm/build.sh
+++ nsm/build.sh
@@ -2,18 +2,12 @@
 
 set -e 
 
-error_handler() {
-    echo "Error occurred in script at line $1"
-    exit 1
-}
-trap 'error_handler $LINENO' ERR
-
 docker build -t nsm . > /dev/null 2>&1
-nitro-cli build-enclave --docker-uri nsm --output-file out.eif > /dev/null 2>&1
+nitro-cli build-enclave --docker-uri nsm --output-file out.eif 
 nitro-cli run-enclave \
     --cpu-count 1 \
     --memory 8000 \
     --eif-path out.eif \
     --enclave-cid 4 \
-    --debug-mode > /dev/null 2>&1
+    --debug-mode
 nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID") 
