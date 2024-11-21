1e6acfabab12fbfa17e4eb8f972cfbf4f13d3f4b
Philip Holm <philipbaltzersen@gmail.com>
2024-11-21 13:29:56 +0100

    More memory

--- nsm/build.sh
+++ nsm/build.sh
@@ -6,7 +6,7 @@ docker build -t nsm .
 nitro-cli build-enclave --docker-uri nsm --output-file nsm.eif
 nitro-cli run-enclave \
     --cpu-count 1 \
-    --memory 3072 \
+    --memory 8000 \
     --eif-path nsm.eif \
     --enclave-cid 4 \
     --debug-mode
