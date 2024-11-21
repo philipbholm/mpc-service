dbc89b248dc0a29a1877400926d669f2e6f6e5bd
Philip Holm <philipbaltzersen@gmail.com>
2024-11-21 17:02:17 +0100

    Add build script

---
+++ proxy/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e 
+
+docker build -t server .
+nitro-cli build-enclave --docker-uri server --output-file out.eif 
+nitro-cli run-enclave \
+    --cpu-count 1 \
+    --memory 8000 \
+    --eif-path out.eif \
+    --enclave-cid 4 \
+    --debug-mode
+nitro-cli console --enclave-id $(nitro-cli describe-enclaves | jq -r ".[0].EnclaveID") 
