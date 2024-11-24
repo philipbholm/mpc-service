4911761da093ad046aa81510b46a06e59c2686ca
Philip Holm <philipbaltzersen@gmail.com>
2024-11-24 11:41:54 +0100

    Add example for randomness

---
+++ nsm-go/example/rand/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "crypto/rand"
+    "fmt"
+    "math/big"
+    "github.com/hf/nsm"
+)
+
+func generateBigPrime() (*big.Int, error) {
+    sess, err := nsm.OpenDefaultSession()
+    defer sess.Close()
+
+    if nil != err {
+        return nil, err
+    }
+
+    return rand.Prime(sess, 2048)
+}
+
+func main() {
+	prime, err := generateBigPrime()
+	if nil != err {
+		panic(err)
+	}
+
+	fmt.Println(prime)
+}
